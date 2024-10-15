f8a0ece392a3ac7f5c4c48510db31284ce532698
Andrea Botello <andrea.botellor@misena.edu.co>
2024-10-15 09:25:03 -0500

    Entrega Repo

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -18,28 +18,28 @@ export default {
       tema: 'Técnicas y estrategias de formación',
       titulo: 'Cuestionario',
       introduccion:
-        '<b> Objetivo:</b> Validar el conocimiento adquirido sobre técnicas y estrategias de formación.',
+        '<b>Objetivo:</b> Validar el conocimiento adquirido sobre técnicas y estrategias de formación.',
       barajarPreguntas: false,
       preguntas: [
         {
           id: 1,
           texto:
-            'Las técnicas de formación se enfocan en métodos concretos, mientras que las estrategias educativas combinan diversas técnicas para lograr un aprendizaje eficaz y adaptado a los estudiantes.',
+            'Las técnicas de formación se enfocan exclusivamente en transmitir conocimientos, sin impacto en habilidades o actitudes.',
           imagen: require('@/assets/curso/temas/ad/1.png'),
           barajarRespuestas: false,
           opciones: [
-            { id: 'a', texto: 'Verdadero', esCorrecta: true },
-            { id: 'b', texto: 'Falso', esCorrecta: false },
+            { id: 'a', texto: 'Verdadero', esCorrecta: false },
+            { id: 'b', texto: 'Falso', esCorrecta: true },
           ],
           mensaje_correcto:
-            'Excelente trabajo. Se observa claridad en la identificación de técnicas y estrategias de formación.',
+            ' ¡Felicitaciones! Ha demostrado un excelente dominio de las técnicas y estrategias de formación. Su comprensión de las herramientas pedagógicas es admirable. ¡Gran trabajo!',
           mensaje_incorrecto:
-            'Respuesta incorrecta, revise nuevamente el contenido del componente formativo.',
+            'Respuesta incorrecta, revise nuevamente el contenido del componente formativo. Gracias por el esfuerzo. Es una oportunidad para mejorar su comprensión de las técnicas pedagógicas.',
         },
         {
           id: 2,
           texto:
-            'Las técnicas de formación son conjuntos de métodos, secuencias y procedimientos cuidadosamente diseñados para orientar los procesos educativos y asegurar una pedagogía tanto eficaz como eficiente.',
+            'Las técnicas individuales en la formación fomentan el esfuerzo personal y la motivación intrínseca.',
           imagen: require('@/assets/curso/temas/ad/2.png'),
           barajarRespuestas: false,
           opciones: [
@@ -47,14 +47,14 @@ export default {
             { id: 'b', texto: 'Falso', esCorrecta: false },
           ],
           mensaje_correcto:
-            'Excelente trabajo. Se observa claridad en la identificación de técnicas y estrategias de formación.',
+            ' ¡Felicitaciones! Ha demostrado un excelente dominio de las técnicas y estrategias de formación. Su comprensión de las herramientas pedagógicas es admirable. ¡Gran trabajo!',
           mensaje_incorrecto:
-            'Respuesta incorrecta, revise nuevamente el contenido del componente formativo.',
+            'Respuesta incorrecta, revise nuevamente el contenido del componente formativo. Gracias por el esfuerzo. Es una oportunidad para mejorar su comprensión de las técnicas pedagógicas. ',
         },
         {
           id: 3,
           texto:
-            'La implementación adecuada de estas técnicas, adaptada a cada contexto específico, NO es fundamental para transmitir conocimientos, ni para fomentar el desarrollo integral de habilidades y actitudes en los estudiantes.',
+            'El mentefacto es una técnica pedagógica utilizada para visualizar relaciones causales entre eventos.',
           imagen: require('@/assets/curso/temas/ad/3.png'),
           barajarRespuestas: false,
           opciones: [
@@ -62,14 +62,14 @@ export default {
             { id: 'b', texto: 'Falso', esCorrecta: true },
           ],
           mensaje_correcto:
-            'Excelente trabajo. Se observa claridad en la identificación de técnicas y estrategias de formación.',
+            ' ¡Felicitaciones! Ha demostrado un excelente dominio de las técnicas y estrategias de formación. Su comprensión de las herramientas pedagógicas es admirable. ¡Gran trabajo!',
           mensaje_incorrecto:
-            'Respuesta incorrecta, revise nuevamente el contenido del componente formativo.',
+            'Respuesta incorrecta, revise nuevamente el contenido del componente formativo. Gracias por el esfuerzo. Es una oportunidad para mejorar su comprensión de las técnicas pedagógicas. ',
         },
         {
           id: 4,
           texto:
-            'El objetivo principal de las técnicas de formación es la transmisión de conocimientos, la enseñanza de nuevas habilidades y la modificación de actitudes.',
+            'La red causal organiza eventos en una secuencia lógica, mostrando sus relaciones de causa y efecto.',
           imagen: require('@/assets/curso/temas/ad/4.png'),
           barajarRespuestas: false,
           opciones: [
@@ -77,14 +77,74 @@ export default {
             { id: 'b', texto: 'Falso', esCorrecta: false },
           ],
           mensaje_correcto:
-            'Excelente trabajo. Se observa claridad en la identificación de técnicas y estrategias de formación.',
+            ' ¡Felicitaciones! Ha demostrado un excelente dominio de las técnicas y estrategias de formación. Su comprensión de las herramientas pedagógicas es admirable. ¡Gran trabajo!',
           mensaje_incorrecto:
-            'Respuesta incorrecta, revise nuevamente el contenido del componente formativo.',
+            'Respuesta incorrecta, revise nuevamente el contenido del componente formativo. Gracias por el esfuerzo. Es una oportunidad para mejorar su comprensión de las técnicas pedagógicas. ',
         },
         {
           id: 5,
           texto:
-            'La importancia de las técnicas de formación radica en su implementación planificada, evitando la improvisación en el aula y asegurando que se realicen las necesarias estimulaciones o retroalimentaciones de contenido para optimizar los resultados de aprendizaje.',
+            'El diagrama de Venn se utiliza únicamente en matemáticas, sin aplicaciones en otros campos educativos.',
+          imagen: require('@/assets/curso/temas/ad/1.png'),
+          barajarRespuestas: false,
+          opciones: [
+            { id: 'a', texto: 'Verdadero', esCorrecta: false },
+            { id: 'b', texto: 'Falso', esCorrecta: true },
+          ],
+          mensaje_correcto:
+            ' ¡Felicitaciones! Ha demostrado un excelente dominio de las técnicas y estrategias de formación. Su comprensión de las herramientas pedagógicas es admirable. ¡Gran trabajo!',
+          mensaje_incorrecto:
+            'Respuesta incorrecta, revise nuevamente el contenido del componente formativo. Gracias por el esfuerzo. Es una oportunidad para mejorar su comprensión de las técnicas pedagógicas. ',
+        },
+        {
+          id: 6,
+          texto:
+            'Las técnicas de formación grupales facilitan el sentido de pertenencia entre los participantes.',
+          imagen: require('@/assets/curso/temas/ad/2.png'),
+          barajarRespuestas: false,
+          opciones: [
+            { id: 'a', texto: 'Verdadero', esCorrecta: true },
+            { id: 'b', texto: 'Falso', esCorrecta: false },
+          ],
+          mensaje_correcto:
+            ' ¡Felicitaciones! Ha demostrado un excelente dominio de las técnicas y estrategias de formación. Su comprensión de las herramientas pedagógicas es admirable. ¡Gran trabajo!',
+          mensaje_incorrecto:
+            'Respuesta incorrecta, revise nuevamente el contenido del componente formativo. Gracias por el esfuerzo. Es una oportunidad para mejorar su comprensión de las técnicas pedagógicas. ',
+        },
+        {
+          id: 7,
+          texto:
+            'El organizador cíclico es una técnica ideal para representar ciclos que se repiten de manera continua.',
+          imagen: require('@/assets/curso/temas/ad/3.png'),
+          barajarRespuestas: false,
+          opciones: [
+            { id: 'a', texto: 'Verdadero', esCorrecta: true },
+            { id: 'b', texto: 'Falso', esCorrecta: false },
+          ],
+          mensaje_correcto:
+            ' ¡Felicitaciones! Ha demostrado un excelente dominio de las técnicas y estrategias de formación. Su comprensión de las herramientas pedagógicas es admirable. ¡Gran trabajo!',
+          mensaje_incorrecto:
+            'Respuesta incorrecta, revise nuevamente el contenido del componente formativo. Gracias por el esfuerzo. Es una oportunidad para mejorar su comprensión de las técnicas pedagógicas. ',
+        },
+        {
+          id: 8,
+          texto:
+            'Las estrategias didácticas no consideran la participación activa del estudiante en el proceso de aprendizaje.',
+          imagen: require('@/assets/curso/temas/ad/4.png'),
+          barajarRespuestas: false,
+          opciones: [
+            { id: 'a', texto: 'Verdadero', esCorrecta: false },
+            { id: 'b', texto: 'Falso', esCorrecta: true },
+          ],
+          mensaje_correcto:
+            ' ¡Felicitaciones! Ha demostrado un excelente dominio de las técnicas y estrategias de formación. Su comprensión de las herramientas pedagógicas es admirable. ¡Gran trabajo!',
+          mensaje_incorrecto:
+            'Respuesta incorrecta, revise nuevamente el contenido del componente formativo. Gracias por el esfuerzo. Es una oportunidad para mejorar su comprensión de las técnicas pedagógicas. ',
+        },
+        {
+          id: 9,
+          texto:
+            'El pensamiento lateral está vinculado con la creatividad y la generación de nuevas ideas.',
           imagen: require('@/assets/curso/temas/ad/1.png'),
           barajarRespuestas: false,
           opciones: [
@@ -92,14 +152,14 @@ export default {
             { id: 'b', texto: 'Falso', esCorrecta: false },
           ],
           mensaje_correcto:
-            'Excelente trabajo. Se observa claridad en la identificación de técnicas y estrategias de formación.',
+            ' ¡Felicitaciones! Ha demostrado un excelente dominio de las técnicas y estrategias de formación. Su comprensión de las herramientas pedagógicas es admirable. ¡Gran trabajo!',
           mensaje_incorrecto:
-            'Respuesta incorrecta, revise nuevamente el contenido del componente formativo.',
+            'Respuesta incorrecta, revise nuevamente el contenido del componente formativo. Gracias por el esfuerzo. Es una oportunidad para mejorar su comprensión de las técnicas pedagógicas. ',
         },
         {
-          id: 6,
+          id: 10,
           texto:
-            'Técnicas grupales o colectivas, No facilitan la construcción del conocimiento a través de la interacción social, ni fomentan un sentido de pertenencia entre los participantes.',
+            'El pensamiento lateral está vinculado con la creatividad y la generación de nuevas ideas.',
           imagen: require('@/assets/curso/temas/ad/2.png'),
           barajarRespuestas: false,
           opciones: [
@@ -107,13 +167,13 @@ export default {
             { id: 'b', texto: 'Falso', esCorrecta: true },
           ],
           mensaje_correcto:
-            'Excelente trabajo. Se observa claridad en la identificación de técnicas y estrategias de formación.',
+            ' ¡Felicitaciones! Ha demostrado un excelente dominio de las técnicas y estrategias de formación. Su comprensión de las herramientas pedagógicas es admirable. ¡Gran trabajo!',
           mensaje_incorrecto:
-            'Respuesta incorrecta, revise nuevamente el contenido del componente formativo.',
+            'Respuesta incorrecta, revise nuevamente el contenido del componente formativo. Gracias por el esfuerzo. Es una oportunidad para mejorar su comprensión de las técnicas pedagógicas. ',
         },
       ],
       mensaje_final_aprobado:
-        '¡Excelente! Lo felicito, ha superado la actividad y demuestra sólidos conocimientos sobre el componente formativo.',
+        ' ¡Excelente trabajo, continúe con ese gran esfuerzo!',
       mensaje_final_reprobado:
         'No ha superado la actividad. Le recomendamos volver a revisar el componente formativo e intentar nuevamente la actividad didáctica.',
     },
